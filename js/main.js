e9310ff5eb6715190d2bc01b29011834a854de86
songjiayang <songjiayang@weidaxue.me>
2014-08-25 17:21:27 +0800

    add sub email request

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function validateEmail(email) { 
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+} 
+
+$(document).ready(function(){
+  $('.btn-submit').click( function(evt){
+    email = $('#email').val()
+    if(validateEmail(email)) {
+      $.ajax({
+        type: 'POST',
+        crossDomain: true,
+        url: 'http://www.zuoyouba.com/sub_emails',
+        data: { "email" : email } ,
+        dataType: 'json',
+        success: function(responseData, textStatus, jqXHR) {
+          var value = responseData.someKey;
+        },
+        error: function(a, b, c) {
+          $('#email').val("")
+          $('.alert').removeClass('hide')
+        }
+      });
+    }
+  });
+});
